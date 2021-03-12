ad17b8213e01584daa0d7aeda16d4dd4cc04debf
Gregor Bachmann <gregor.bachmann@unibas.ch>
2021-03-12 21:26:55 +0100

    an added comment to check if Intellij works fine

--- src/main/java/ch/unibas/dmi/dbis/cs108/example/HelloWorld.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/HelloWorld.java
@@ -4,7 +4,7 @@ package ch.unibas.dmi.dbis.cs108.example;
  * A simple HelloWorld class.
  */
 public class HelloWorld {
-
+  // a simple comment as a simple commit to check if intellij works
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
