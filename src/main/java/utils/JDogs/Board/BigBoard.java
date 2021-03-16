bda8fcf18f89fa3c728a42df8a30f887e4f110a5
Gregor Bachmann <gregor.bachmann@unibas.ch>
2021-03-16 14:31:53 +0100

    -added BoardGameState.java; this will be used as a memory for the game state

--- src/main/java/utils/JDogs/Board/BigBoard.java
+++ src/main/java/utils/JDogs/Board/BigBoard.java
@@ -1,7 +1,7 @@
 package utils.JDogs.Board;
 //represents the big board with 96 NormIndentations + 6*4 BoxIndentations + 6*4 GoalIndentations
 public class BigBoard extends Board {
-
+    //at least 16 * ca. 24
     public static final int BOARD_LENGTH = 0;
     public static final int BOARD_WIDTH = 0;
 
--- src/main/java/utils/JDogs/Board/SmallBoard.java
+++ src/main/java/utils/JDogs/Board/SmallBoard.java
@@ -1,7 +1,7 @@
 package utils.JDogs.Board;
 //this file represents the small board
 public class SmallBoard extends Board {
-
+    //at least 16*16 Squares
     public static final int BOARD_LENGTH = 0;
     public static final int BOARD_WIDTH = 0;
 
