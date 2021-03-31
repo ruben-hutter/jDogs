3dd272ab41bbec8d38481363d6b303c693ca9746
johannameyer <johanna.meyer@unibas.ch>
2021-03-31 15:08:05 +0200

    Documented commands in ServerMenuProtocol.java

--- src/main/java/JDogs/ServerClientEnvironment/ServerSide/ServerMenuProtocol.java
+++ src/main/java/JDogs/ServerClientEnvironment/ServerSide/ServerMenuProtocol.java
@@ -5,16 +5,45 @@ package JDogs.ServerClientEnvironment.ServerSide;
  * received by the server and which deal with the menu/lobby.
  */
 public enum ServerMenuProtocol {
-
+    /**
+     * Command to set username.
+     */
     USER,
+    /**
+     * Command to show a list of active users.
+     */
     ACTI,
+    /**
+     * Command to show the open games which can be joined.
+     */
     SESS,
+    /**
+     * Command to send a private message to a player.
+     */
     WCHT,
+    /**
+     * Command to request the game statistics.
+     */
     STAT,
+    /**
+     * Command to disconnect client from the server.
+     */
     QUIT,
+    /**
+     * Command to confirm whether the game should be started under the given conditions.
+     */
     STAR,
+    /**
+     * Command to start a new game.
+     */
     PLAY,
+    /**
+     * Command to choose the game mode: Number of players, composition of the team, design of the board.
+     */
     MODE,
+    /**
+     * Command to tell if a client has joined the game or not.
+     */
     JOIN;
 
 
