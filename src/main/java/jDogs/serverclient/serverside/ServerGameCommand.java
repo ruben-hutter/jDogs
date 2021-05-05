242a042c6240b144f455c29b23eb8cc3177ce3e9
Gregor Bachmann <gregor.bachmann@unibas.ch>
2021-05-05 12:25:38 +0200

    forgot a String to initialize

--- src/main/java/jDogs/serverclient/serverside/ServerGameCommand.java
+++ src/main/java/jDogs/serverclient/serverside/ServerGameCommand.java
@@ -170,6 +170,7 @@ public class ServerGameCommand {
                                     serverConnection.sendToClient("TURN");
                                     break;
                                 case 9:
+                                    String winner = null;
                                     Server.getInstance().storeGame(mainGame.getGameId(), winner);
                                     break;
                             }
