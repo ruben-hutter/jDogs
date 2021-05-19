727bd86f0aca57ea3bac5b085b5cb18a33e77141
ruben.hutter <ruben.hutter@stud.unibas.ch>
2021-05-19 09:53:57 +0200

    implemented a complete card set in the code. now all colors of the deck are displayed in game

--- src/main/java/jDogs/gui/CardUrl.java
+++ src/main/java/jDogs/gui/CardUrl.java
@@ -1,65 +1,100 @@
 package jDogs.gui;
 
+import java.util.Random;
+
+/**
+ * Chooses the correct image for a given card
+ */
 public class CardUrl {
+
     public static String getURL(String card) {
+        Random random = new Random();
         String url = null;
+        int randNum;
 
         switch (card) {
 
             case "ACEE":
-                url = "/cards/AC.png";
+                String[] aces = {"/playing_cards/Ac.png", "/playing_cards/Ad.png", "/playing_cards/Ah.png", "/playing_cards/As.png"};
+                randNum = random.nextInt(aces.length);
+                url = aces[randNum];
+                //url = "/cards/AC.png";
                 break;
 
             case "TWOO":
-                url = "/cards/2C.png";
+                String[] twos = {"/playing_cards/2c.png", "/playing_cards/2d.png", "/playing_cards/2h.png", "/playing_cards/2s.png"};
+                randNum = random.nextInt(twos.length);
+                url = twos[randNum];
                 break;
 
             case "THRE":
-                url = "/cards/3C.png";
+                String[] threes = {"/playing_cards/3c.png", "/playing_cards/3d.png", "/playing_cards/3h.png", "/playing_cards/3s.png"};
+                randNum = random.nextInt(threes.length);
+                url = threes[randNum];
                 break;
 
             case "FOUR":
-                url = "/cards/4C.png";
+                String[] fours = {"/playing_cards/4c.png", "/playing_cards/4d.png", "/playing_cards/4h.png", "/playing_cards/4s.png"};
+                randNum = random.nextInt(fours.length);
+                url = fours[randNum];
                 break;
 
             case "FIVE":
-                url = "/cards/5C.png";
+                String[] fives = {"/playing_cards/5c.png", "/playing_cards/5d.png", "/playing_cards/5h.png", "/playing_cards/5s.png"};
+                randNum = random.nextInt(fives.length);
+                url = fives[randNum];
                 break;
 
             case "SIXX":
-                url = "/cards/6C.png";
+                String[] sixes = {"/playing_cards/6c.png", "/playing_cards/6d.png", "/playing_cards/6h.png", "/playing_cards/6s.png"};
+                randNum = random.nextInt(sixes.length);
+                url = sixes[randNum];
                 break;
 
             case "SEVE":
-                url = "/cards/7C.png";
+                String[] sevens = {"/playing_cards/7c.png", "/playing_cards/7d.png", "/playing_cards/7h.png", "/playing_cards/7s.png"};
+                randNum = random.nextInt(sevens.length);
+                url = sevens[randNum];
                 break;
 
             case "EIGT":
-                url = "/cards/8C.png";
+                String[] eights = {"/playing_cards/8c.png", "/playing_cards/8d.png", "/playing_cards/8h.png", "/playing_cards/8s.png"};
+                randNum = random.nextInt(eights.length);
+                url = eights[randNum];
                 break;
 
             case "NINE":
-                url = "/cards/9C.png";
+                String[] nines = {"/playing_cards/9c.png", "/playing_cards/9d.png", "/playing_cards/9h.png", "/playing_cards/9s.png"};
+                randNum = random.nextInt(nines.length);
+                url = nines[randNum];
                 break;
 
             case "TENN":
-                url = "/cards/10C.png";
+                String[] tens = {"/playing_cards/10c.png", "/playing_cards/10d.png", "/playing_cards/10h.png", "/playing_cards/10s.png"};
+                randNum = random.nextInt(tens.length);
+                url = tens[randNum];
                 break;
 
             case "QUEN":
-                url = "/cards/QC.png";
+                String[] queens = {"/playing_cards/Qc.png", "/playing_cards/Qd.png", "/playing_cards/Qh.png", "/playing_cards/Qs.png"};
+                randNum = random.nextInt(queens.length);
+                url = queens[randNum];
                 break;
 
             case "JACK":
-                url = "/cards/JC.png";
+                String[] jacks = {"/playing_cards/Jc.png", "/playing_cards/Jd.png", "/playing_cards/Jh.png", "/playing_cards/Js.png"};
+                randNum = random.nextInt(jacks.length);
+                url = jacks[randNum];
                 break;
 
             case "JOKE":
-                url = "/cards/red_back.png";
+                url = "/playing_cards/Joker.png";
                 break;
 
             case "KING":
-                url = "/cards/KC.png";
+                String[] kings = {"/playing_cards/Kc.png", "/playing_cards/Kd.png", "/playing_cards/Kh.png", "/playing_cards/Ks.png"};
+                randNum = random.nextInt(kings.length);
+                url = kings[randNum];
                 break;
 
             default:
